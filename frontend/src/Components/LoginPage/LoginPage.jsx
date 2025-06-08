5b8f6eee645dc67687ed81d02ab775a98a1ea1b3
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-06-09 03:38:13 +0545

    Make login work on client-side

--- frontend/src/Components/LoginPage/LoginPage.jsx
+++ frontend/src/Components/LoginPage/LoginPage.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import "./LoginPage.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function LoginPage() {
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginClick = () => {
+    fetch(`${import.meta.env.VITE_BACKEND_SERVER_URL}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        phone: phone,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => {
+            localStorage.setItem("token", data.token);
+            window.location.href = "/home";
+          });
+        } else {
+          alert("Login failed. Please check your credentials.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error during login:", error);
+        alert("An error occurred during login. Please try again later.");
+      });
+  };
+
   return (
     <>
       <div className="top-logo">
         <img
-              src="../src/assets/img/logo/logo-full-transparent.png"
-              alt="Cost-Effective"
-            />
-        <a href="/home">Bali<span>Rakshak.</span></a>
+          src="../src/assets/img/logo/logo-full-transparent.png"
+          alt="Cost-Effective"
+        />
+        <a href="/home">
+          Bali<span>Rakshak.</span>
+        </a>
       </div>
       <div className="login-container">
         <h2>Log In</h2>
@@ -19,9 +52,11 @@ function LoginPage() {
           <div className="form-group">
             <label>Phone Number</label>
             <input
-              type="text"
+              type="tel"
               placeholder="Enter your phone number"
               name="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
             />
           </div>
 
@@ -31,15 +66,19 @@ function LoginPage() {
               type="password"
               placeholder="Enter your password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
         </form>
 
-        <button className="login-btn">Log in</button>
+        <button className="login-btn" onClick={handleLoginClick}>
+          Log in
+        </button>
 
-       <p className="signup-link">
-        Don’t have an account? <Link to="/signup">Sign up</Link>
-      </p>
+        <p className="signup-link">
+          Don’t have an account? <Link to="/signup">Sign up</Link>
+        </p>
       </div>
     </>
   );
